e19e60905821bb598cb62838e949c7f2fec888cb
jatinder <jatindersingh33@gmail.com>
2017-08-16 13:12:09 +1000

    Make header navigation working, Bind item details as per item in item list, Added functionality to ingredient in shoping list

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,15 +1,22 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component, OnInit , EventEmitter, Output} from '@angular/core';
+import { Recipe } from './recipe.model'
 @Component({
   selector: 'app-recipe-list',
   templateUrl: './recipe-list.component.html',
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
-  recipes =[];
+  @Output() recipeWasSelected= new EventEmitter<Recipe>();
+  recipes: Recipe[] = [
+    new Recipe('A Test Recipe','This is simply a test','https://ciaoflorentina.com/wp-content/uploads/2011/09/Tuna-bites.jpg'),
+    new Recipe('A Test Recipe 1','This is simply a test 1 ','https://ciaoflorentina.com/wp-content/uploads/2011/09/Tuna-bites.jpg')
+  ];
   constructor() { }
 
   ngOnInit() {
   }
 
+  onRecipeSelected(recipe:Recipe){
+    this.recipeWasSelected.emit(recipe);
+  }
 }
