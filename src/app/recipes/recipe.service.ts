c84d34abd8dcf25fa58787867ec2e2a87d0b6710
jatinder <jatindersingh33@gmail.com>
2017-08-20 21:42:47 +1000

    Created Service for shopping list. Adding ingredient to shopping list by using service is working now

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,19 +1,47 @@
-import {EventEmitter, Output} from '@angular/core';
+import {EventEmitter, Output, Injectable} from '@angular/core';
 import { Recipe } from './recipe-list/recipe.model';
+import {Ingredient} from '../shared/ingredient.model';
+import {ShoppingListService} from '../shopping-list/shopping-list.service';
+
+@Injectable()
 
 export class RecipeService{
 
 @Output() recipeSelected = new EventEmitter<Recipe>();
 
     private recipes: Recipe[] = [
-    new Recipe('A Test Recipe','This is simply a test','https://ciaoflorentina.com/wp-content/uploads/2011/09/Tuna-bites.jpg'),
-    new Recipe('A Test Recipe 1','This is simply a test 1 ','https://ciaoflorentina.com/wp-content/uploads/2011/09/Tuna-bites.jpg')
+    new Recipe(
+        'Chicken Tikka',
+        'Chicken tikka is a chicken dish originating in the Punjab region of the Indian Subcontinent; the dish is popular in India and Pakistan.',
+        'http://indianhealthyrecipes.com/wp-content/uploads/2013/02/chicken-tikka-recipe-step-6.jpg',
+        [
+            new Ingredient('Chicken',15),
+            new Ingredient('Tomatoes',4),
+            new Ingredient('Onion',5),
+        ]),
+    new Recipe(
+        'Butter Chicken',
+        'Butter chicken or murgh makhani is a dish from the Indian subcontinent of chicken in a mildly spiced curry sauce.',
+        'https://i.ytimg.com/vi/a03U45jFxOI/maxresdefault.jpg',
+        [
+            new Ingredient('chicken',15),
+            new Ingredient('Cream',1),
+            new Ingredient('Onion',5),
+            new Ingredient('Tomatoes',8),
+        ]
+    )
   ];
+
+  constructor(private slService: ShoppingListService ){
+
+  }
 
 getRecipes(){
     return this.recipes.slice();
 }
 
-
+addIngredientToShoppingList(ingredients : Ingredient[]){
+this.slService.addIngredients(ingredients);
+}
 
 }
--- src/app/shopping-list/shopping-edit/shopping-edit.component.ts
+++ src/app/shopping-list/shopping-edit/shopping-edit.component.ts
@@ -1,5 +1,6 @@
-import { Component, OnInit, ViewChild , ElementRef, EventEmitter, Output} from '@angular/core';
+import { Component, OnInit, ViewChild , ElementRef} from '@angular/core';
 import {Ingredient} from '../../shared/ingredient.model';
+import {ShoppingListService} from '../shopping-list.service';
 
 @Component({
   selector: 'app-shopping-edit',
@@ -10,19 +11,16 @@ export class ShoppingEditComponent implements OnInit {
 @ViewChild('nameInput') nameInputRef: ElementRef;
 @ViewChild('amountInput') amountInputRef: ElementRef;
 
-@Output() ingredientAdded = new EventEmitter<Ingredient>();
-  constructor() { }
+  constructor(private slService: ShoppingListService) { }
 
   ngOnInit() {
   }
 
-  onAdded(){
+  onAddItem(){
     const ingName = this.nameInputRef.nativeElement.value;
     const ingAmount = this.amountInputRef.nativeElement.value;
-    
     const newIngredient = new Ingredient(ingName, ingAmount);
-    this.ingredientAdded.emit(newIngredient);
-
+    this.slService.addIngredient(newIngredient);
   }
 
 }
